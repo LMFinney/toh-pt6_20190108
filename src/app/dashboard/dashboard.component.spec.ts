d397eff3c92b77db169a7f8abda1f26b944b0841
Lance Finney <lance.finney@oasisdigital.com>
2019-01-08 13:41:50 -0600

    Test DashboardComponent as though it's a service

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,40 +1,28 @@
-import { ComponentFixture, TestBed, async } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { of } from 'rxjs';
 
-import { HeroSearchComponent } from '../hero-search/hero-search.component';
 import { HeroService } from '../hero.service';
 import { HEROES } from '../mock-heroes';
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async(() => {
     const svcSpy = jasmine.createSpyObj<HeroService>(['getHeroes']);
     svcSpy.getHeroes.and.returnValue(of(HEROES));
 
     TestBed.configureTestingModule({
-      declarations: [
-        DashboardComponent,
-        HeroSearchComponent
-      ],
-      imports: [RouterTestingModule.withRoutes([])],
       providers: [
+        DashboardComponent,
         { provide: HeroService, useValue: svcSpy }
       ],
-      // schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ]
-    })
-      .compileComponents();
-
+    });
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = TestBed.get(DashboardComponent);
+    component.ngOnInit();
   });
 
   it('should be created', () => {
@@ -44,11 +32,6 @@ describe('DashboardComponent', () => {
   it('should skip the first hero', () => {
     expect(component.heroes.length).toBe(4);
     expect(component.heroes[0]).toBe(HEROES[1]);
-  });
-
-  it('should display 4 links', () => {
-    expect(fixture.nativeElement.querySelectorAll('a').length).toBe(4);
-    // expect(fixture.debugElement.queryAll(By.css('a')).length).toBe(4);
   });
 
 });
